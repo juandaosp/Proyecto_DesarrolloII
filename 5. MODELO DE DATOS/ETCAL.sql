6049541161bfe0725588e4b67efeb867992fc8de
Juan David Ospina Vasquez <juandaosp12@gmail.com>
2012-11-11 17:21:05 -0500

    MODIFICADO GITIGNORE Y ETCAL.SQL DEL MODELO DE DATOS

--- 5. MODELO DE DATOS/ETCAL.sql
+++ 5. MODELO DE DATOS/ETCAL.sql
@@ -1,5 +1,5 @@
-DROP TABLE empleado CASCADE;
-DROP TABLE bud CASCADE;
+﻿DROP TABLE empleado CASCADE;
+DROP TABLE bus CASCADE;
 DROP TABLE ruta CASCADE;
 DROP TABLE programacion CASCADE;
 DROP TABLE estacion CASCADE;
@@ -16,7 +16,7 @@ nombre VARCHAR(50) NOT NULL,
 login VARCHAR(20) NOT NULL,
 contrasena VARCHAR(30) NOT NULL,
 eps VARCHAR(40) NOT NULL,
-salario INT (100) NOT NULL,
+salario INT  NOT NULL,
 licencia VARCHAR(50) NOT NULL,
 genero CHAR(1) NOT NULL,
 estado_civil VARCHAR(20) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE bus
 (
 matricula VARCHAR(30) PRIMARY KEY,
 estado VARCHAR(30) NOT NULL,
-a�o VARCHAR(10) NOT NULL,
+año VARCHAR(10) NOT NULL,
 fabricante VARCHAR(40) NOT NULL,
 capacidad VARCHAR(30) NOT NULL,
 cilindrinaje VARCHAR(40) NOT NULL,
@@ -49,13 +49,7 @@ CREATE TABLE programacion
 hora_inicio VARCHAR(10) NOT NULL,
 hora_fin VARCHAR(10) NOT NULL,
 fecha DATE NOT NULL, 
-id_empleado INT NOT NULL,
-nombre_ruta VARCHAR(30) NOT NULL,
-matricula_bus VARCHAR(40) NOT NULL,
-descripcion VARCHAR(50) NOT NULL,
-FOREIGN KEY (id_empleado) REFERENCES empleado(id),
-FOREIGN KEY (nombre_ruta) REFERENCES ruta(nombre_ruta),
-FOREIGN KEY (matricula_bus) REFERENCES bus(matricula)
+id_empleado VARCHAR(50)
 );
 
 CREATE TABLE estacion
@@ -63,7 +57,7 @@ CREATE TABLE estacion
 nombre_estacion VARCHAR(40) PRIMARY KEY,
 ubicacion VARCHAR(50) NOT NULL,
 estado VARCHAR(30) NOT NULL,
-id_empleado INT NOT NULL,
+id_empleado VARCHAR(50) NOT NULL,
 FOREIGN KEY (id_empleado) REFERENCES empleado(id)
 );
 
@@ -75,6 +69,16 @@ FOREIGN KEY (nombre_ruta) REFERENCES ruta(nombre_ruta),
 FOREIGN KEY (nombre_estacion) REFERENCES estacion(nombre_estacion)
 );
 
+CREATE TABLE tarjeta
+(
+pin INT PRIMARY KEY NOT NULL,
+costo VARCHAR(20) NOT NULL,
+tipo VARCHAR(30) NOT NULL,
+estado VARCHAR(30) NOT NULL,
+pasajes VARCHAR (10) NOT NULL,
+nombre_estacion VARCHAR (40) NOT NULL,
+FOREIGN KEY (nombre_estacion) REFERENCES estacion(nombre_estacion)
+);
 CREATE TABLE usuario
 (
 id_usuario INT PRIMARY KEY,
@@ -99,16 +103,7 @@ FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
 FOREIGN KEY (nombre_estacion) REFERENCES estacion(nombre_estacion)
 );
 
-CREATE TABLE tarjeta
-(
-pin INT PRIMARY KEY NOT NULL,
-costo VARCHAR(20) NOT NULL,
-tipo VARCHAR(30) NOT NULL,
-estado VARCHAR(30) NOT NULL,
-pasajes VARCHAR (10) NOT NULL,
-nombre_estacion VARCHAR (40) NOT NULL,
-FOREIGN KEY (nombre_estacion) REFERENCES estacion(nombre_estacion)
-);
+
 
 CREATE TABLE usuario_ingresa_estacion
 (
@@ -120,15 +115,4 @@ FOREIGN KEY (nombre_estacion) REFERENCES estacion(nombre_estacion),
 FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT* FROM EMPLEADO;
